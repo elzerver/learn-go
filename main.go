9c513fd9d32300ea4ba40bcc0355677e571127ed
aldo <test@tester.com>
2021-02-14 00:01:31 +0100

    define global variable and define a a new function

--- main.go
+++ main.go
@@ -2,10 +2,20 @@ package main
 
 import "fmt"
 
+var z = 43
+
 func main() {
 	// short declaration operator :=
 	x := 42
 	fmt.Println(x)
 	person := "James Bond"
 	fmt.Println(person)
+	
+	fmt.Println(z)
+
+	foo()
+}
+
+func foo() {
+	fmt.Println("again:", z)
 }
