05f3b3369de62c71ad69627e6dfc28381d5f9d8c
aldo <test@tester.com>
2021-02-14 13:34:03 +0100

    Adding the solved exercise1 to the repo

---
+++ exercise1.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func exercise() {
+	x := 42 
+	y := "James Bond" 
+	z := true
+    // Print in a single line fmt.Println(x, y, z)
+	fmt.Println(x, y, z)
+	fmt.Println(x)
+	fmt.Println(y)
+	fmt.Println(z)
+}
+
+func main() {
+        exercise()
+}
