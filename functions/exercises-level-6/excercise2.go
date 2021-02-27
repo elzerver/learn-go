627faec5704627bb1d5da9eae414291307e68cb9
aldo <test@tester.com>
2021-02-27 12:41:39 +0100

    exercise 2 solved

---
+++ functions/exercises-level-6/excercise2.go
@@ -0,0 +1,40 @@
+package main
+
+import "fmt"
+
+// Create a func with the idenfier foo
+// takes in a variadic parameter of int
+// pass in a value of type []int into your func (unfurl the []int)
+// returns the sum of all values of type int passed in
+
+// create a function with the identifier bar
+// that takes the parameter of type []int
+// returns the sum of all values of type int passed in
+
+func foo(values ...int) int {
+	var total int
+	total = 0
+	for _, v := range values {
+		// fmt.Println(v)
+		total += v
+	}
+	// fmt.Println(total)
+	// fmt.Printf("%T\n", total)
+	return total
+}
+
+func bar(v ...int) int {
+	// fmt.Printf("%T\n", v)
+	var total int
+	total = foo(v...)
+	// fmt.Printf("%T\n", total)
+	return total
+}
+
+func main() {
+	numbers := []int{1, 2, 3, 4, 5, 6, 7, 8, 9}
+	// foo(numbers...)
+	var value int
+	value = bar(numbers...)
+	fmt.Println(value)
+}
